ecd552cddc4760f9d9d3cd5c4346803770ecbe66
r0manski <rp.publicbox@gmail.com>
2017-08-19 20:54:25 +1000

    Mongoose is added and DB connection file created. DB connection termination triggers were added.

---
+++ app_server/models/db.js
@@ -0,0 +1,56 @@
+var mongoose = require('mongoose');
+
+//defining and connecting to Mongo DB
+var dbURI = 'mongodb://localhost/bouquet';
+mongoose.connect(dbURI);
+
+//monitor connection
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose has successfully connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose has disconnected');
+});
+
+// capturing termination events
+
+
+//function to close Mongoose connection
+
+var delicateShutdown = function( msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose has disconnected because ' + msg);
+        callback();
+    });
+};
+
+//adding event listeners
+
+//listen for SIGUSR2(nodemon event), send message to delicateShutdown and callback to kill process, emitting SIGUSR2 again
+process.once('SIGUSR2', function () {
+    delicateShutdown('nodemon restart', function () {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+//listen for SIGINT emmited on application termination
+
+process.on('SIGINT', function () {
+    delicateShutdown('app is terminated', function () {
+        process.exit(0);
+    });
+});
+
+//listen for SIGTERM emmited when HEROKU shuts down process
+
+process.on('SIGTERM', function () {
+    delicateShutdown("Heroku has terminated app ", function () {
+        process.exit(0);
+    });
+});
