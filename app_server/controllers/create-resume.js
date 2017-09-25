5bdfc877d86819cd65c72231bc98bc767ae5d7de
r0manski <rp.publicbox@gmail.com>
2017-09-25 16:19:50 +1000

    minor routing fixes

--- app_server/controllers/create-resume.js
+++ app_server/controllers/create-resume.js
@@ -2,6 +2,10 @@ var request = require('request');
 var apiOptions = {
     server : "http://localhost:3000"
 };
+//use different server for Heroku
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = "https://aipbouquet.herokuapp.com/";
+}
 
 /* function to render the create-resume page */
 
